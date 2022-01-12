0b72eba039f7e34c9adfa52666b02df54ae26d9b
KhaledAchech <achechkhaled@gmail.com>
2022-01-12 11:28:51 +0100

    db connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require ('express');
+const mongoose = require("mongoose");
 
 const app = express();
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5050;
+
+//connect to DB
+mongoose
+  .connect(process.env.DB_CONNNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Mongo is up."))
+  .catch((err) => console.log("Mongo is Down. Raison :", err));
 
 // Routes
 app.get('/', (req,res) => {
