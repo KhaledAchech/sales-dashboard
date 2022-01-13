c82a6463a462d9c8536966a70cd4c6aa89f6e50c
KhaledAchech <achechkhaled@gmail.com>
2022-01-13 14:40:18 +0100

    backend/auth

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const express = require ('express');
+const express = require ("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5050;
@@ -17,10 +24,12 @@ mongoose
   .then(() => console.log("Mongo is up."))
   .catch((err) => console.log("Mongo is Down. Raison :", err));
 
-// Routes
+//Routes
 app.get('/', (req,res) => {
     res.send("<h1> we're home ^^ </h1>")
 })
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
 
 // Start the server
 app.listen(PORT, () => {
---
+++ server/routes/users.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {User, validate} = require("../models/user");
+const bcrypt = require('bcrypt');
+
+router.post("/", async (req,res) => {
+    try{
+        const { err } = validate(req.body);
+        if (err)
+        {
+            return res.status(400).send({ message:err.details[0].message });
+        }
+
+        const user = await User.findOne ({ email: req.body.email });
+
+        if (user)
+        {
+            return res.status(409).send({ message:"User with given email already exists."})
+        }
+
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        await new User({...req.body, password: hashPassword}).save();
+        res.status(201).send({message: "User created successfully"});
+
+    } catch(err){
+        res.status(500).send({message:"Internal server error : " + err.details[0].message});
+    }
+})
+
+module.exports = router;
